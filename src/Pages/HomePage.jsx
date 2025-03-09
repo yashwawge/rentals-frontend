80f5e81cfcfa735df9bd34a19890c04ce8885f4d
Yash Wawge <yashwawge01@gmail.com>
2025-03-09 09:51:16 +0530

    Added home ui

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slide from '../components/slide'
+import Category from '../components/Category'
+
 const HomePage = () => {
   return (
     <>
       <Navbar />
+      <Slide />
+      <Category/>
     </>
   )
 }
